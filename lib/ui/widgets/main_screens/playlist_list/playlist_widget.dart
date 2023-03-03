5e31daaf21c3e8272095392a262e8ffad7f80c9c
Amir Kharisov <amirkharisov545@Gmail.com>
2023-03-03 21:42:12 +0300

    Added PlayListWidget

--- lib/ui/widgets/main_screens/playlist_list/playlist_widget.dart
+++ lib/ui/widgets/main_screens/playlist_list/playlist_widget.dart
@@ -6,35 +6,60 @@ class PlaylistListWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          onPressed: () {},
-          icon: const Image(
-            image: AssetImage(AppImages.search),
-          ),
-        ),
-        title: const Text('Playlist'),
-        centerTitle: true,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Image(
-              image: AssetImage(AppImages.add),
-            ),
-          ),
-        ],
+    return GridView.builder(
+      shrinkWrap: true,
+      physics: const BouncingScrollPhysics(),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+        childAspectRatio: 0.8,
       ),
-      body: Column(
-        children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(12),
-            child: const Image(
-              image: AssetImage(AppImages.playlistImage),
-            ),
+      itemCount: 11,
+      itemBuilder: (BuildContext context, int index) {
+        return Padding(
+          padding: const EdgeInsets.all(8),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(12),
+                    child: const Image(
+                      image: AssetImage(AppImages.playlistImage),
+                    ),
+                  ),
+                  const SizedBox(height: 8),
+                  const Text(
+                    'Happiers',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  const Text(
+                    '45 songs',
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                  ),
+                ],
+              ),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(12),
+                child: Material(
+                  color: Colors.transparent,
+                  child: InkWell(
+                    onTap: () {},
+                  ),
+                ),
+              )
+            ],
           ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
